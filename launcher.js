a7dce7d914549da2cddaf9c2166ad175e7b2c5a2
Iyappan Mani <iyappan.mani@outlook.com>
2023-11-14 18:04:53 -0700

    Initial Commit

--- launcher.js
+++
@@ -1,5 +0,0 @@
-function calculate() {
-    var buyingPrice = document.getElementById("buyingPrice").innerText;
-    var sellingPrice = document.getElementById("sellingPrice").innerText;
-    document.getElementById("difference").innerHTML =  buyingPrice - sellingPrice;
-}
